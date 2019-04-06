bf1f7181bc1d52f7bf5835652b7b25b35aba2cb7
Joey Hwang <hwangjt@uci.edu>
2019-04-06 08:35:06 -0700

    Initial Commit

---
+++ tipCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipCalc
+//
+//  Created by Joey Hwang on 4/6/19.
+//  Copyright © 2019 Joey Hwang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
